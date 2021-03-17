1c21c0ba8f9a8f0e26900b3ba1f706b94abfcd0c
zingimmick <zingimmick@outlook.com>
2021-03-17 09:21:28 +0000

    Refactor ServiceProvider (#6)

--- src/LikeServiceProvider.php
+++ src/LikeServiceProvider.php
@@ -4,48 +4,10 @@ declare(strict_types=1);
 
 namespace LaravelInteraction\Like;
 
-use Illuminate\Support\ServiceProvider;
+use LaravelInteraction\Support\InteractionList;
+use LaravelInteraction\Support\InteractionServiceProvider;
 
-class LikeServiceProvider extends ServiceProvider
+class LikeServiceProvider extends InteractionServiceProvider
 {
-    public function boot(): void
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes(
-                [
-                    $this->getConfigPath() => config_path('like.php'),
-                ],
-                'like-config'
-            );
-            $this->publishes(
-                [
-                    $this->getMigrationsPath() => database_path('migrations'),
-                ],
-                'like-migrations'
-            );
-            if ($this->shouldLoadMigrations()) {
-                $this->loadMigrationsFrom($this->getMigrationsPath());
-            }
-        }
-    }
-
-    public function register(): void
-    {
-        $this->mergeConfigFrom($this->getConfigPath(), 'like');
-    }
-
-    protected function getConfigPath(): string
-    {
-        return __DIR__ . '/../config/like.php';
-    }
-
-    protected function getMigrationsPath(): string
-    {
-        return __DIR__ . '/../migrations';
-    }
-
-    private function shouldLoadMigrations(): bool
-    {
-        return (bool) config('like.load_migrations');
-    }
+    protected $interaction = InteractionList::LIKE;
 }
